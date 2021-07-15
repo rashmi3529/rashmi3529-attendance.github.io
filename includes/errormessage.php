64b19bbba2e1af5cd5aa26ef2fcaa460bb5896c3
Rashmi parmar <rashmi9893079175@gmail.com>
2021-07-15 19:48:50 +0530

    rashmi3529-attendance

---
+++ includes/errormessage.php
@@ -0,0 +1,3 @@
+<div class="alert alert-danger" role="alert">
+  Operation encountered in Errror. please try again later.
+</div>
---
+++ includes/header.php
@@ -0,0 +1,34 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.0/dist/css/bootstrap.min.css" 
+    integrity="sha384-B0vP5xmATw1+K9KRQjQERJvTumQW0nPEzvF6L/Z6nronJ3oUOFUFpCjEUQouq2+l" crossorigin="anonymous">
+    <link rel="stylesheet" href="//code.jquery.com/ui/1.12.1/themes/base/jquery-ui.css">
+
+    <link rel="stylesheet" href="css/site.css" />
+
+    <title>ATTENDANCE -<?php echo $title?> </title>
+  </head>
+  <body>
+      <div class="container">
+      <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+           <a class="navbar-brand" href="index.php">IT Conference</a>
+           <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+              <span class="navbar-toggler-icon"></span>
+            </button>
+           <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+              <div class="navbar-nav">
+                <a class="nav-item nav-link active" href="index.php">Home <span class="sr-only">(current)</span></a>
+                <a class="nav-item nav-link" href="viewrecords.php">View Attendees</a>
+                
+              </div>
+            </div>
+      </nav>
+      
+      <br/>
+   
